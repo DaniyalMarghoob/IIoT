02d5d4657b9461de67e77d03962db4d260bd3025
DaniyalMarghoob <daniyalmarghoob@gmail.com>
2017-10-12 16:19:38 +0300

    Updated values.py

--- MQTTPythonMockData/values.py
+++ MQTTPythonMockData/values.py
@@ -1,22 +1,14 @@
 import retain
-#import http as info
-
-
 data=retain.readData()
-#number_of_boxes=raw_input('Number of boxes: ').lower()
 
 if(data[0]==0):
-
  import config 
  print("Initializing")
- counter=-1
- serviceInterval=1000
  client=raw_input("Client Name: ").lower()
  name=raw_input("Machine Name: ").lower()
  serial=raw_input("Serial Number: ").lower()
  location = raw_input("Location: ").lower()
- config.subTopic=config.subBaseURL+client+"/"+location+"/"+name+"/"+serial
- config.pubTopic=config.pubBaseURL+client+"/"+serial
+ 
  print("ini")
 
 if(data[0]==1):
@@ -29,5 +21,8 @@ if(data[0]==1):
     name=data[1]
     serial=data[3]
     location = data[4]
-    config.subTopic = config.subBaseURL + client + "/" + location + "/" + name + "/" + serial
-    config.pubTopic = config.pubBaseURL + client + "/" + serial
+    
+ counter=-1
+ serviceInterval=1000
+ config.subTopic=config.subBaseURL+client+"/"+location+"/"+name+"/"+serial
+ config.pubTopic=config.pubBaseURL+client+"/"+serial
